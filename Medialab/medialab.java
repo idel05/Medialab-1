21467573702465dc79607342cbe1323aa4c96a7b
ellamaryp16 <42983257+ellamaryp16@users.noreply.github.com>
2018-09-10 13:10:37 -0500

    Medialab

---
+++ Medialab/medialab.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class medialab here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class medialab
+{
+      public static void main ()
+  {
+    System.out.println("Welcome to your media library.");
+    }
+}
