8b407f7195f911a74098a9c29a6613946f72877d
idel05 <42978812+idel05@users.noreply.github.com>
2018-09-11 12:26:43 -0500

    Update medialab.java

--- Medialab/medialab.java
+++ Medialab/medialab.java
@@ -2,13 +2,13 @@
 /**
  * Write a description of class medialab here.
  *
- * @author (your name)
+ * @author (Isabella and Ella)
  * @version (a version number or a date)
  */
 public class medialab
 {
       public static void main ()
   {
-    System.out.println("Welcome to your media library, my dears.");
+    System.out.println("Welcome to your media library.");
     }
 }
